a2134ad0465ead8b0f8477e4c013bd1111721e53
seafewd <alexander.korpas@protonmail.com>
2021-10-19 14:10:52 +0200

    fixed forkAfter

--- src/main/amazed/solver/ForkJoinSolver.java
+++ src/main/amazed/solver/ForkJoinSolver.java
@@ -33,7 +33,7 @@ public class ForkJoinSolver extends SequentialSolver {
     // thread safe ArrayList
     private final CopyOnWriteArrayList<ForkJoinTask<List<Integer>>> threads = new CopyOnWriteArrayList<>();
 
-    private static AtomicBoolean GOAL_FOUND = new AtomicBoolean(false);
+    private static final AtomicBoolean GOAL_FOUND = new AtomicBoolean(false);
 
     /**
      * initialize with empty thread safe data structures
@@ -154,25 +154,23 @@ public class ForkJoinSolver extends SequentialSolver {
             }
         }
 
-        // if we only have one node to check, 'just do it'
+        // if number of unvisited nodes is less than forkAfter value, then 'just do it'
         // else fork with current data
-        if (unvisited.size() == 1) {
+        if (unvisited.size() < forkAfter) {
             Integer nextNode = unvisited.iterator().next();
             maze.move(player, nextNode);
             return parallelSearch(nextNode);
         } else
-            for (Integer nextNode : unvisited) {
+            for (Integer nextNode : unvisited)
                 threads.add(new ForkJoinSolver(maze, nextNode, forkAfter, visited, predecessor).fork());
-            }
 
         // go through all lists of neighbors in threads doing work
         // join with respective partial result
         List<Integer> path = null;
         for (ForkJoinTask<List<Integer>> thread : threads) {
             List<Integer> partialResult = thread.join();
-            if (partialResult != null) {
+            if (partialResult != null)
                 path = partialResult;
-            }
         }
         // return path result
         return path;
